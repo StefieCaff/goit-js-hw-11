7163cf6a886d65396d0ed37664611c92bdd1907b
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-16 19:19:24 -0800

    update src files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+
+var API_KEY = '33708941-9afad2bda68efbaf1594840f2';
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});
