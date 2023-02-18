87bb253ffedfa07f19f6b0a9d52110950b3a8834
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-18 13:15:15 -0800

    markup logic
    step one

--- src/index.js
+++ src/index.js
@@ -4,10 +4,59 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const userInput = document.querySelector('.searchQuery');
+const API_KEY = '33708941-9afad2bda68efbaf1594840f2';
+const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + userInput + "&image_type=photo&orientation=horizontal&safesearch=true";
 
+const renderGallery = images => {
 
-// var API_KEY = '33708941-9afad2bda68efbaf1594840f2';
-// var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+  const gallery = document.querySelector('.gallery');
+
+  const markup = images
+    .map((image) => {
+  
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads
+      } = imgParams;
+      return (gallery.innerHTML =
+        `
+          <div class="photo-card">
+            <a class="gallery__item" href="${largeImageURL}">
+              <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes: ${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>Views: ${views}</b>
+              </p>
+              <p class="info-item">
+                <b>Comments: ${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>Downloads: ${downloads}</b>
+              </p>
+            </div>
+          </div>
+
+        `
+      )
+    })
+    .join("");
+  gallery.innerHTML = markup;
+};  
+
+{/* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" loading="lazy" />
+</a> */}
+
 // $.getJSON(URL, function(data){
 // if (parseInt(data.totalHits) > 0)
 //     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
