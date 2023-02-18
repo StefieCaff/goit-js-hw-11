c860b20af64e29392c508dc57d7e2a16edc761f6
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-18 13:37:58 -0800

    file structure
    file structure not working scss is not showing in github page visit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + userInput + "&im
 const renderGallery = images => {
 
   const gallery = document.querySelector('.gallery');
-
   const markup = images
     .map((image) => {
   
@@ -51,11 +50,7 @@ const renderGallery = images => {
     })
     .join("");
   gallery.innerHTML = markup;
-};  
-
-{/* <a class="gallery__item" href="large-image.jpg">
-  <img class="gallery__image" src="small-image.jpg" alt="Image description" loading="lazy" />
-</a> */}
+};
 
 // $.getJSON(URL, function(data){
 // if (parseInt(data.totalHits) > 0)
@@ -67,38 +62,7 @@ const renderGallery = images => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-//--------------------template for card------------------------
-`<a>
-  <div class="photo-card">
-    <img src="" alt="" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-      </p>
-    </div>
-  </div>
-</a>`
-
 //--------------initialize------------------------------
 
 //simple lightbox image library
-//const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, throttleInterval: 100 });
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, throttleInterval: 100 });
