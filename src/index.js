98072c541c87120bbb10875a29bfd400a70524d2
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-18 14:06:18 -0800

    more file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-//------------------imports/require------------------------------
+//------------------local---------------------------------
 import './css/styles.css';
+import renderGallery from './render-gallery';
 
+//------------------libraries------------------------------
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -10,49 +12,7 @@ const userInput = document.querySelector('.searchQuery');
 const API_KEY = '33708941-9afad2bda68efbaf1594840f2';
 const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + userInput + "&image_type=photo&orientation=horizontal&safesearch=true";
 
-const renderGallery = images => {
 
-  const gallery = document.querySelector('.gallery');
-  const markup = images
-    .map((image) => {
-  
-      const {
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads
-      } = imgParams;
-      return (gallery.innerHTML =
-        `
-          <div class="photo-card">
-            <a class="gallery__item" href="${largeImageURL}">
-              <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>
-            <div class="info">
-              <p class="info-item">
-                <b>Likes: ${likes}</b>
-              </p>
-              <p class="info-item">
-                <b>Views: ${views}</b>
-              </p>
-              <p class="info-item">
-                <b>Comments: ${comments}</b>
-              </p>
-              <p class="info-item">
-                <b>Downloads: ${downloads}</b>
-              </p>
-            </div>
-          </div>
-
-        `
-      )
-    })
-    .join("");
-  gallery.innerHTML = markup;
-};
 
 // $.getJSON(URL, function(data){
 // if (parseInt(data.totalHits) > 0)
