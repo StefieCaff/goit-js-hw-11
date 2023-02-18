b91cea11bf495f79a70b910eb831ade7ff3d1f62
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-17 17:58:33 -0800

    styles

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
-var API_KEY = '33708941-9afad2bda68efbaf1594840f2';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-    console.log('No hits');
-});
+// var API_KEY = '33708941-9afad2bda68efbaf1594840f2';
+// var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
 
 
 
@@ -29,20 +29,27 @@ else
 
 
 //--------------------template for card------------------------
-`<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
+`<a>
+  <div class="photo-card">
+    <img src="" alt="" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+      </p>
+    </div>
   </div>
-</div>`
+</a>`
+
+//--------------initialize------------------------------
+
+//simple lightbox image library
+//const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, throttleInterval: 100 });
